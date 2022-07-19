56bc1a15cfedd5b37d5229c41101dfbe722a9d45
Sergey-Yurtaev <engineerk2@mail.ru>
2022-07-19 23:52:28 +0500

    Initial Commit

---
+++ Contact(SwiftUI)HW.3.5/Contact_SwiftUI_HW_3_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_SwiftUI_HW_3_5App.swift
+//  Contact(SwiftUI)HW.3.5
+//
+//  Created by  Sergey Yurtaev on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Contact_SwiftUI_HW_3_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
